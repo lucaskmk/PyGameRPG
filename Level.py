ee6c8c743e3dbe78a5672b58283035e131c636f2
LKenji <lucaskamikawa@gmail.com>
2023-05-17 18:19:06 -0300

    world map

--- Level.py
+++ Level.py
@@ -1,2 +1,17 @@
 import pygame
-class Levelç 
+from settings import *
+class Level:
+    def __init__(self):
+        # superficie do display
+        self.display_surface = pygame.display.get_surface()
+        # gropos das sprites
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+        #sprite setup
+        self.create_map()
+    def create_map(self):
+        for row in WORLD_MAP:
+            print(row)
+    def run_game(self):
+        pass
+    
